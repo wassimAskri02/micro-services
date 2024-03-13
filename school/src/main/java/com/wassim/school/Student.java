c9f9b5dfe9f93c50b60e23256a08e151a12e7e72
wassimaskri02 <wissask1234@gmail.com>
2024-03-13 10:00:51 +0100

    projet micro services wassim

---
+++ school/src/main/java/com/wassim/school/Student.java
@@ -0,0 +1,15 @@
+package com.wassim.school;
+
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Student {
+
+    private String firstname;
+    private String lastname;
+    private String email;
+}
